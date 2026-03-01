0780f5696e7e3e0d3efb0f08216cae7bc33ff480
Mounika-satani <mounika@weiteredge.com>
2026-03-02 00:36:47 +0530

    done

--- migrations/20260226120000-add-comments-enabled-to-startup-posts.js
+++ migrations/20260226120000-add-comments-enabled-to-startup-posts.js
@@ -1,15 +1,24 @@
 'use strict';
 
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn('startup_posts', 'comments_enabled', {
-            type: Sequelize.BOOLEAN,
-            allowNull: false,
-            defaultValue: true,
-        });
-    },
+  async up(queryInterface, Sequelize) {
 
-    async down(queryInterface) {
-        await queryInterface.removeColumn('startup_posts', 'comments_enabled');
+    const table = await queryInterface.describeTable('startup_posts');
+
+    if (!table.comments_enabled) {
+      await queryInterface.addColumn('startup_posts', 'comments_enabled', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      });
     }
+  },
+
+  async down(queryInterface) {
+    const table = await queryInterface.describeTable('startup_posts');
+
+    if (table.comments_enabled) {
+      await queryInterface.removeColumn('startup_posts', 'comments_enabled');
+    }
+  }
 };
