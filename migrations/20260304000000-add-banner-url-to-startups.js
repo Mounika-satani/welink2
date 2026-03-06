40d98fe00cbf6cff6f7822557d46035e0c988edc
Mounika-satani <mounika@weiteredge.com>
2026-03-06 17:19:13 +0530

    done

--- migrations/20260304000000-add-banner-url-to-startups.js
+++ migrations/20260304000000-add-banner-url-to-startups.js
@@ -2,14 +2,26 @@
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.addColumn('startups', 'banner_url', {
-            type: Sequelize.TEXT,
-            allowNull: true,
-            defaultValue: null,
-        });
+
+        const table = await queryInterface.describeTable('startups');
+
+        if (!table.banner_url) {
+            await queryInterface.addColumn('startups', 'banner_url', {
+                type: Sequelize.TEXT,
+                allowNull: true,
+                defaultValue: null,
+            });
+        }
+
     },
 
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.removeColumn('startups', 'banner_url');
+
+        const table = await queryInterface.describeTable('startups');
+
+        if (table.banner_url) {
+            await queryInterface.removeColumn('startups', 'banner_url');
+        }
+
     }
 };
